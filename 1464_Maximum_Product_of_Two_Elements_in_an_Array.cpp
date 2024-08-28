9652438a3810ec40f8105d8fccdd7ae30987370f
Asik8 <motasembillah84@gmail.com>
2024-08-28 10:17:53 +0600

    p5 done

--- 1464_Maximum_Product_of_Two_Elements_in_an_Array.cpp
+++ 1464_Maximum_Product_of_Two_Elements_in_an_Array.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& v) {
-        sort(v.begin(),v.end());
-        int n = v.size();
-        return (v[n-2]-1)*(v[n-1]-1);
+    int maxProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        return (nums[n-2]-1)*(nums[n-1]-1);
     }
 };
